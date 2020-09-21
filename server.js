7e1dd5ddcad655bdcf62be56c9590e1d1b78d73e
rybalkoko <rybalkoko@alfastrah.ru>
2020-09-21 09:08:28 +0300

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Express
 let express = require('express');
+let cors = require('cors')
 let cookieParser = require('cookie-parser');
 let app = express();
+
+app.use(cors());
 
 const path = require('path');
 
@@ -17,6 +20,29 @@ app.use(bodyParser.json());
 let morgan = require('morgan');
 app.use(morgan('dev'));
 app.use(cookieParser());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get("/work-time/:regionCode", function (req, res, next) {
+    next();
+}, (req, res) => {
+    res.json({
+        urlParams: req.params.regionCode,
+        birthday: '20.02.1988',
+        category: "INDIVIDUAL",
+        city: 'Собачья нора',
+        email: "mail@mail.mail",
+        surname: "Фамилия",
+        name: "Имя",
+        patronymic: "Отчество",
+        phone: '9296816253',
+        status: "CHILDREN",
+    })
+});
 
 // Routes
 app.get("*", (req, res, next) => {
